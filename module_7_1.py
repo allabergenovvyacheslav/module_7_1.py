2287a44535fd7c3799bae08f5996e78794d747a6
Vyacheslav <slawushkin@mail.ru>
2024-07-31 09:21:05 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 # Задача "Учёт товаров":
 # Необходимо реализовать 2 класса Product и Shop, с помощью которых будет производиться запись в файл
 # с продуктами.
@@ -20,48 +18,66 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return print(f'{self.name}, {self.weight}, {self.category}')
-
-
+        return f'{self.name}, {self.weight}, {self.category}'
 
 # Объекты класса Shop будут создаваться следующим образом - Shop() и обладать следующими свойствами:
+
 # Инкапсулированный атрибут __file_name = 'products.txt'.
+
 # Метод get_products(self), который считывает всю информацию из файла __file_name, закрывает его
 # и возвращает единую строку со всеми товарами из файла __file_name.
+
 # Метод add(self, *products), который принимает неограниченное количество объектов класса Product.
 # Добавляет в файл __file_name каждый продукт из products, если его ещё нет в файле (по названию).
 # Если такой продукт уже есть, то не добавляет и выводит строку 'Продукт <название> уже есть в магазине'.
 
+
 class Shop:
     def __init__(self):
-        self.__file_name = 'text2.txt'
+        self.__file_name = 'products.txt'
 
     def get_products(self):
-        self.__file_name = 'text2.txt'
         file = open(self.__file_name, 'r')
-        pprint(file.read())
+        food = file.read()
         file.close()
+        return food
 
     def add(self, *products):
-        for i in products:
-            if i.name not in self.get_products():
-                file = open(self.__file_name, 'a')
-                file.write(i.__str__() + '\n')
+        for food in products:
+            if food.name in self.get_products():
+                print(f'Продукт {food.name} уже есть в магазине')
             else:
-                print(f'Продукт {i.name} уже есть в магазине')
-
+                file = open(self.__file_name, 'a')
+                file.write(f'{food} \n')
+                file.close()
 
 
 if __name__ == '__main__':
-    p1 = Product('Potato', 50.0, 'Vagetables')
-    print(p1)
-    # s1 = Shop()
-    # p1 = Product('Potato', 50.5, 'Vegetables')
-    # p2 = Product('Spaghetti', 3.4, 'Groceries')
-    # p3 = Product('Potato', 5.5, 'Vegetables')
-#
-    # print(p2)  # __str__
-#
-    # s1.add(p1, p2, p3)
-#
-    # print(s1.get_products())
+    s1 = Shop()
+    p1 = Product('Potato', 50.5, 'Vegetables')
+    p2 = Product('Spaghetti', 3.4, 'Groceries')
+    p3 = Product('Potato', 5.5, 'Vegetables')
+
+    print(p2)  # __str__
+
+    s1.add(p1, p2, p3)
+
+    print(s1.get_products())
+
+# Вывод на консоль:
+
+# Первый запуск:
+
+# Spaghetti, 3.4, Groceries
+# Продукт Potato уже есть в магазине
+# Potato, 50.5, Vegetables
+# Spaghetti, 3.4, Groceries
+
+# Второй запуск:
+
+# Spaghetti, 3.4, Groceries
+# Продукт Potato уже есть в магазине
+# Продукт Spaghetti уже есть в магазине
+# Продукт Potato уже есть в магазине
+# Potato, 50.5, Vegetables
+# Spaghetti, 3.4, Groceries
